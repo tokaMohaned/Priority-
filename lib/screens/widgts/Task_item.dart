4ff63c0c700885475b05e9440690b98818c99a10
tokas <tokaseliman@gmail.com>
2023-05-21 19:31:28 +0300

    UI task tab finish

--- lib/screens/widgts/Task_item.dart
+++ lib/screens/widgts/Task_item.dart
@@ -1,19 +1,64 @@
 import 'package:date_picker_timeline/extra/color.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:todo/styles/app-color.dart';
 
 class TaskItem extends StatelessWidget {
   const TaskItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      children: [
-        Container(
-          height: 100,
-          width: 5,
-          //color: AppColors.l,
-        )
-      ],
-    );
+    return
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+           // margin: EdgeInsets.symmetric(vertical: 8),
+            elevation: 12,
+            shape: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(
+                color: Colors.transparent
+              )
+            ),
+            child: Row(
+             // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 100,
+                  width: 5,
+                  decoration: BoxDecoration(
+                    color: lightColor,
+                    borderRadius: BorderRadius.circular(4) ,
+
+                  ),
+                  margin: EdgeInsets.only(top:6,bottom: 22 ),
+
+                ),
+              SizedBox(width: MediaQuery.of(context).size.width*.05,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,//to begin in the same point
+                  children: [
+                    Text("Task title ",style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: lightColor,
+                    ),),
+                    Text("Task Description ",style: Theme.of(context).textTheme.bodyMedium,)
+                  ],
+                ),
+                Spacer(),
+                 Container(
+                   margin: EdgeInsets.only(right: 16),//Empty space to surround the decoration and child.
+                   padding: EdgeInsets.symmetric(horizontal:18,vertical: 3 ),
+                     decoration: BoxDecoration(
+                       color:lightColor,
+                       borderRadius: BorderRadius.circular(12)
+                     ),
+                     child:
+                 Icon(Icons.done,color: Colors.white,
+                     size:30),),
+              ],//row
+
+    ),
+          ),
+        );
   }
 }
--- lib/screens/widgts/task_tab.dart
+++ lib/screens/widgts/task_tab.dart
@@ -1,6 +1,8 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/screens/widgts/Task_item.dart';
+import 'package:todo/styles/app-color.dart';
 
 class TaskTab extends StatefulWidget {
   const TaskTab({Key? key}) : super(key: key);
@@ -10,28 +12,38 @@ class TaskTab extends StatefulWidget {
 }
 
 class _TaskTabState extends State<TaskTab> {
+  DateTime date=DateTime.now();
   @override
   Widget build(BuildContext context) {
     return  Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // children: <Widget>[
-      //   DatePicker(
-      //     DateTime.now(),
-      //     initialSelectedDate: DateTime.now(),
-      //     selectionColor: Colors.black,
-      //     selectedTextColor: Colors.white,
-      //     onDateChange: (date) {
-      //       // New date selected
-      //       setState(() {
-      //         _selectedValue = date;
-      //       });
-      //     },
-      //   ),
-        // Expanded(child:
-        // ListView.builder(
-        //     itemBuilder: TaskItem())
-        // )
-      //],
+      children: [
+        DatePicker(
+          DateTime.now(),
+          initialSelectedDate: DateTime.now(),
+          selectionColor: lightColor,
+          height: 100,
+         // inactiveDates: [DateTime.now().add(Duration(days: 2))],
+
+          selectedTextColor: Colors.white,
+          onDateChange: (newDate) {
+            // New date selected
+            setState(() {
+              date = newDate;
+            });
+          },
+        ),
+        Expanded(child: ListView.builder(
+
+            itemBuilder: (context,index)
+        {
+         return TaskItem();
+        },
+        itemCount: 10,),
+        )
+      ],
+
+
+
     );
 
 
